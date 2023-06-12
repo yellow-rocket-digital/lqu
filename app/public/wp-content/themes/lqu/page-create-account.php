9e6a90915d20d8c13b71f1bc1541876927d9800b
dari-davis <hello@darielledavis.com>
2023-06-12 18:06:37 -0400

    updates account registration form:

--- app/public/wp-content/themes/lqu/page-create-account.php
+++ app/public/wp-content/themes/lqu/page-create-account.php
@@ -11,34 +11,45 @@ $top_navigation_color_theme = 'tan';
 include('section-top-navigation.php');
 ?>
 
-<form name="registerform" id="registerform"
-	action="<?php echo esc_url( site_url( 'wp-login.php?action=register', 'login_post' ) ); ?>" method="post"
-	novalidate="novalidate">
-	<p>
-		<label for="user_login"><?php _e( 'Username' ); ?></label>
-		<input type="text" name="user_login" id="user_login" class="input"
-			value="<?php echo esc_attr( wp_unslash( $user_login ) ); ?>" size="20" autocapitalize="off"
-			autocomplete="username" />
-	</p>
-	<p>
-		<label for="user_email"><?php _e( 'Email' ); ?></label>
-		<input type="email" name="user_email" id="user_email" class="input"
-			value="<?php echo esc_attr( wp_unslash( $user_email ) ); ?>" size="25" autocomplete="email" />
-	</p>
+<div class="woocommerce-page">
+	<section class="generic-page">
+		<div class="woocommerce">
+			<div class="generic-content row col-md-6 mx-auto">
+				<div class="p-3">
+					<h1>Request An Account</h1>
+					<form name="registerform" id="registerform" class="register-form"
+						action="<?php echo esc_url( site_url( 'wp-login.php?action=register', 'login_post' ) ); ?>" method="post"
+						novalidate="novalidate">
+						<p class="visually-hidden">
+							<input type="text" name="user_login" id="user_login" class="input"
+								value="<?php echo esc_attr( wp_unslash( $user_login ) ); ?>" size="20" autocapitalize="off"
+								autocomplete="username" />
+							<label for="user_login"><?php _e( 'Username' ); ?></label>
+						</p>
+						<p>
+							<input type="email" name="user_email" id="user_email" class="input" value="<?php echo esc_attr( wp_unslash( $user_email ) ); ?>" size="25" autocomplete="email" />
+							<label for="user_email"><?php _e( 'Email Address' ); ?></label>
 
-	<?php do_action( 'register_form' ); ?>
+						</p>
 
-	<p id="reg_passmail">
-		<?php _e( 'Registration confirmation will be emailed to you.' ); ?>
-	</p>
-	<br class="clear" />
-	<input type="hidden" name="redirect_to" value="<?php echo esc_attr( $redirect_to ); ?>" />
-	<p class="submit">
-		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large"
-			value="<?php esc_attr_e( 'Register' ); ?>" />
-	</p>
+						<?php do_action( 'register_form' ); ?>
 
-</form>
+						<p id="reg_passmail">
+							<?php _e( 'Registration confirmation will be emailed to you.' ); ?>
+						</p>
+						<br class="clear" />
+						<input type="hidden" name="redirect_to" value="<?php echo esc_attr( $redirect_to ); ?>" />
+						<p class="submit">
+							<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large"
+								value="<?php esc_attr_e( 'Request Account' ); ?>" />
+						</p>
+
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php
 get_footer();
