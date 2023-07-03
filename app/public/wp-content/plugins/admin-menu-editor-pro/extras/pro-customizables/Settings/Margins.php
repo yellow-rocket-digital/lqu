ae33b063c0f70fd28dd4aeed60e8e92c01b20ab8
dari-davis <hello@darielledavis.com>
2023-07-03 15:16:45 -0400

    removes elementor plugins

--- app/public/wp-content/plugins/admin-menu-editor-pro/extras/pro-customizables/Settings/Margins.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace YahnisElsts\AdminMenuEditor\ProCustomizable\Settings;
-
-class Margins extends SpacingSetting {
-	protected $label = 'Margins';
-	protected $cssPropertyPrefix = 'margin-';
-	//TODO: What about "auto"?
-}
--- app/public/wp-content/plugins/admin-menu-editor-pro/extras/pro-customizables/Settings/Padding.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace YahnisElsts\AdminMenuEditor\ProCustomizable\Settings;
-
-class Padding extends SpacingSetting {
-	protected $label = 'Padding';
-	protected $cssPropertyPrefix = 'padding-';
-}
